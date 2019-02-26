227a2546e29c4f77e3ecef2a0eed69178bdc0b8e
Bryant <cabrera.bryant@gmail.com>
2019-02-25 19:41:00 -0800

    Sets up basic server in app.py.

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+import config
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.run(debug=config.DEBUG, port=config.PORT)
