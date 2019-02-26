192cae4f26ab7aaebbe8c65a95f618f1b5a2c274
Bryant <cabrera.bryant@gmail.com>
2019-02-26 13:36:35 -0800

    Creates users.py rout info in resources directory.

---
+++ resources/users.py
@@ -0,0 +1,33 @@
+from flask import jsonify, Blueprint
+
+from flask_restful import Resource, Api
+
+import models
+
+class UserList(Resource):
+    def get(self):
+        return jsonify({'users': [{'username': 'Bryant'}]})
+
+
+class Boardgame(Resource):
+    def get(self, id):
+        return jsonify({'username': 'Bryant'})
+
+    def put(self, id):
+        return jsonify({'username': 'BryantCabrera'})
+
+    def delete(self, id):
+        return jsonify({'username': 'Bryant Cabrera deleted'})
+
+users_api = Blueprint('resources.users', __name__)
+api = Api(users_api)
+api.add_resource(
+    UserList,
+    '/users',
+    endpoint='users'
+)
+api.add_resource(
+    Boardgame,
+    '/users/<int:id>',
+    endpoint='user'
+)
