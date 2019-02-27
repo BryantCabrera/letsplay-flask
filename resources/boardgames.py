d1c4a20f46e03c51db186cfef3f6b7ea19364dc6
Bryant <cabrera.bryant@gmail.com>
2019-02-26 19:07:40 -0800

    Adds PUT (update) and DELETE (delete) routes for resources/boardgames.py model.  User POST, GET, PUT, and DELETE routes verified with PostMan.

--- resources/boardgames.py
+++ resources/boardgames.py
@@ -19,6 +19,14 @@ boardgame_fields = {
     'description': fields.String,
 }
 
+def boardgame_or_404(boardgame_id):
+    try:
+        boardgame = models.Boardgame.get(models.Boardgame.id == boardgame_id)
+    except models.Boardgame.DoesNotExist:
+        abort(404)
+    else:
+        return boardgame
+
 class BoardgameList(Resource):
     #this is the response to the client
     def __init__(self):
@@ -79,25 +87,95 @@ class BoardgameList(Resource):
         )
         super().__init__()
 
+    #this is one way of incorporating marshal.  Including this marshal without decorater to demonstrate it's the same as with the decorater
     def get(self):
-        return jsonify({'boardgame': [{'title': 'Settlers of Catan'}]})
+        boardgames = [marshal(boardgame, boardgame_fields)
+                   for boardgame in models.Boardgame.select()]
+        return {'boardgame': boardgames}
 
+    @marshal_with(boardgame_fields)
     def post(self):
         args = self.reqparse.parse_args()
         print(args, ' this is args from BoardgameList in boardgames.py')
         boardgame = models.Boardgame.create(**args)
-        return jsonify({'boardgame': [{'title': 'Settlers of Catan'}]})
+        return boardgame
 
 
 class Boardgame(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'title',
+            required=True,
+            help='No title provided.',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'designer',
+            required=False,
+            help='No designer provided.',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'number_of_players_max',
+            required=True,
+            help='No maximum number of players provided.',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'number_of_players_min',
+            required=True,
+            help='No minimum number of players provided.',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'age_min',
+            required=False,
+            help='No minimum age provided.',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'play_time_max',
+            required=True,
+            help='No maximum playtime provided.',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'play_time_min',
+            required=True,
+            help='No minimum playtime provided.',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'img_url',
+            required=True,
+            help='No image url provided.',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'description',
+            required=True,
+            help='No description provided.',
+            location=['form', 'json']
+        )
+        super().__init__()
+
+    @marshal_with(boardgame_fields)
     def get(self, id):
-        return jsonify({'title': 'Settlers of Catan'})
+        return boardgame_or_404(id)
 
+    @marshal_with(boardgame_fields)
     def put(self, id):
-        return jsonify({'title': 'Settlers of Catan: Cities & Knights'})
+        args = self.reqparse.parse_args()
+        query = models.Boardgame.update(**args).where(models.Boardgame.id == id)
+        query.execute()
+        return (models.Boardgame.get(models.Boardgame.id == id), 200)
 
+    @marshal_with(boardgame_fields)
     def delete(self, id):
-        return jsonify({'title': 'Settlers of Catan: Cities & Knights deleted'})
+        query = models.Boardgame.delete().where(models.Boardgame.id == id)
+        query.execute()
+        return 'This boardgame resource was successfully deleted.'
 
 boardgames_api = Blueprint('resources.boardgames', __name__)
 api = Api(boardgames_api)
--- resources/users.py
+++ resources/users.py
@@ -64,6 +64,7 @@ class UserList(Resource):
         # )
         super().__init__()
 
+    #this is one way of incorporating marshal.  Including this marshal without decorater to demonstrate it's the same as with the decorater
     def get(self):
         users = [marshal(user, user_fields)
                    for user in models.User.select()]
