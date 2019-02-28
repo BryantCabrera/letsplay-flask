1d6961712727b80445b1d41d714824fa5c71b1b6
Bryant <cabrera.bryant@gmail.com>
2019-02-28 13:50:49 -0800

    Adds Bryant's code to populate boardgame database with information from boardgame API in models.py.

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 import datetime
+import requests
+import json
 
 from peewee import *
 from flask_bcrypt import generate_password_hash
@@ -11,7 +13,7 @@ class User(UserMixin, Model):
     email = CharField(unique = True)
     password = CharField()
     location = IntegerField(null = True)
-    img_url = CharField(null = True)
+    img_url = CharField(default='./static/imgs/no_img.png', null = True)
     # member_since = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
@@ -38,9 +40,10 @@ class Boardgame(Model):
     designer = CharField()
     number_of_players_max = IntegerField()
     number_of_players_min = IntegerField()
-    age_min = IntegerField()
-    play_time_max = IntegerField()
-    play_time_min = IntegerField()
+    # age_min = IntegerField()
+    # play_time_max = IntegerField()
+    # play_time_min = IntegerField()
+    play_time = IntegerField()
     img_url = CharField()
     description = CharField()
     # created_at = DateTimeField(default=datetime.datetime.now)
@@ -56,7 +59,37 @@ class UserBoardgame(Model):
     class Meta:
         database = DATABASE
 
+def populate():
+    for i in range(35, 101):
+        request = requests.get('https://bgg-json.azurewebsites.net/thing/{number}'.format(number=i))
+        # request = requests.get('https://www.boardgamegeek.com/xmlapi2/thing/id={number}'.format(number=i))
+        # print(json.loads(request.text)['image'])
+        # print(json.loads(request.text))
+        parsed_request = json.loads(request.text)
+        # boardgame = {
+        #     'title': parsed_request['name'],
+        #     'designer': parsed_request['designers'][0],
+        #     'number_of_players_max': parsed_request['maxPlayers'],
+        #     'number_of_players_min': parsed_request['minPlayers'],
+        #     # 'age_min'': parsed_request['name'],
+        #     # 'play_time_max'': parsed_request['name'],
+        #     'play_time': parsed_request['playingTime'],
+        #     'img_url': parsed_request['image'],
+        #     'description': parsed_request['description']
+        # }
+        # print(boardgame)
+        Boardgame.create(title=parsed_request['name'],
+            designer=parsed_request['designers'][0],
+            number_of_players_max=parsed_request['maxPlayers'],
+            number_of_players_min=parsed_request['minPlayers'],
+            # 'age_min'=parsed_request['name'],
+            # 'play_time_max'=parsed_request['name'],
+            play_time=parsed_request['playingTime'],
+            img_url=parsed_request['image'],
+            description=parsed_request['description'])
+
 def initialize():
     DATABASE.connect()
     DATABASE.create_tables([User, Boardgame], safe=True)
+    populate()
     DATABASE.close()
