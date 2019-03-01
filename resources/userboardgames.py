e23867dbe13a3f3369f8cb91105350e835151294
Bryant <cabrera.bryant@gmail.com>
2019-03-01 15:00:36 -0800

    Adds Bryant's join table logic for userboardgames.py that returns a user's boardgames.

--- resources/userboardgames.py
+++ resources/userboardgames.py
@@ -1,7 +1,7 @@
 from flask import jsonify, Blueprint, abort
 from flask_restful import (Resource, Api, reqparse, fields, marshal,
                                marshal_with, url_for)
-from flask_login import login_required
+from flask_login import login_required, current_user
 import models
 
 #what we want to send back when a user is called
@@ -39,10 +39,32 @@ class UserBoardgameList(Resource):
 
     #this is one way of incorporating marshal.  Including this marshal without decorater to demonstrate it's the same as with the decorater
 
+    # def get(self):
+    #     userboardgames = [marshal(userboardgame, userboardgame_fields)
+    #                for userboardgame in models.UserBoardgame.select()]
+    #     return {'userboardgames': userboardgames}
+    @login_required
     def get(self):
-        userboardgames = [marshal(userboardgame, userboardgame_fields)
-                   for userboardgame in models.UserBoardgame.select()]
-        return {'userboardgames': userboardgames}
+        # test = models.Boardgame.select().join(models.UserBoardgame).join(models.User).where(models.User.id == current_user.id)
+        # # args = self.reqparse.parse_args()
+        # query = models.UserBoardgame.select().join(User).where(User.id == current_user.id)
+        # print(query, ' this is query from userboardgames get')
+        # print(str(current_user.id), 'args from get userbg')
+
+
+        userboardgames = models.Boardgame.select().join(models.UserBoardgame).join(models.User).where(models.User.id == current_user.id)
+
+        for boardgame in userboardgames:
+            print(boardgame.title, ' this is test from userboardgames get')
+
+        return userboardgames
+        # userboardgames_ids = [marshal(userboardgame, userboardgame_fields)
+        #            for userboardgame in models.UserBoardgame.select().where(models.UserBoardgame.user == str(current_user.id))]
+        # for userboardgames_id in userboardgames_ids:
+        #     print(userboardgames_id['boardgame'])
+        # userboardgames = [for boardgame in models.Boardgame.select().where(models.Boardgame.id == for userboardgames_id in userboardgames_ids:userboardgames_ids['boardgame'])]
+        
+        # return {'userboardgames': userboardgames}
 
     # @login_required
     # @marshal_with(userboardgame_fields)
