308d0cb3a15a9040b2c23e7467517015e10b1790
Bryant <cabrera.bryant@gmail.com>
2019-03-04 16:31:22 -0800

    updates populate in models.py.

--- models.py
+++ models.py
@@ -49,7 +49,7 @@ class Boardgame(Model):
     number_of_players_min = IntegerField()
     play_time = IntegerField()
     img_url = CharField()
-    description = CharField()
+    description = TextField()
 
     class Meta:
         database = DATABASE
@@ -73,7 +73,7 @@ def populate():
             number_of_players_min=parsed_request['minPlayers'],
             play_time=parsed_request['playingTime'],
             img_url=parsed_request['image'],
-            description='hello')
+            description=parsed_request['description'])
 
 def initialize():
     DATABASE.connect()
